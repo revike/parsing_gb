74f341a1bd24f14b852e3e2a19656b8ebc605a93
revike <revike@ya.ru>
2021-08-15 21:29:26 +0300

    hw_7

--- hw_7/product_scraper/product_scraper/settings.py
+++ hw_7/product_scraper/product_scraper/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for jobparser project
+# Scrapy settings for product_scraper project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,29 +7,34 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'jobparser'
+BOT_NAME = 'product_scraper'
 
-SPIDER_MODULES = ['jobparser.spiders']
-NEWSPIDER_MODULE = 'jobparser.spiders'
+SPIDER_MODULES = ['product_scraper.spiders']
+NEWSPIDER_MODULE = 'product_scraper.spiders'
+
 
 LOG_ENABLED = True
 LOG_LEVEL = 'DEBUG'
-# LOG_FILE = 'logs.txt'
 
+IMAGES_STORE = 'images'
+IMAGES_THUMBS = {
+    'small': (160, 120),
+    'medium': (320, 240)
+}
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
              '(KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8  # 8
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5  # 1.5
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -49,13 +54,13 @@ COOKIES_ENABLED = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'jobparser.middlewares.JobparserSpiderMiddleware': 543,
+#    'product_scraper.middlewares.ProductScraperSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'jobparser.middlewares.JobparserDownloaderMiddleware': 543,
+#    'product_scraper.middlewares.ProductScraperDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -67,7 +72,8 @@ COOKIES_ENABLED = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'jobparser.pipelines.JobparserPipeline': 300,
+   'product_scraper.pipelines.ProductScraperPipeline': 300,
+   'product_scraper.pipelines.LeroyPhotoPipeline': 200,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
